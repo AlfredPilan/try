28af0b3dfbf3980be310e27d8bef1fb37f793e43
AlfredPilan <95341783+AlfredPilan@users.noreply.github.com>
2025-05-05 23:49:55 +0800

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,25 @@
+function homeBtn(){
+    window.location.href="  index.html";
+}
+
+function aboutBtn(){
+    window.location.href="about.html";
+}
+
+function featuredBtn(){
+    window.location.href="featured.html";
+}
+
+function menuBtn(){
+    window.location.href="Menu.html";
+}
+
+function contactBtn(){
+    window.location.href="contact.html";
+}
+function Orderbtn(){
+    alert("THANK YOU FOR YOUR ORDER!:)");
+}
+function messagebtn(){
+    alert("Received messages.");
+}
